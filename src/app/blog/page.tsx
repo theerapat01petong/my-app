24b42431b719ceef753599fb1bae01dab1cc990c
theerapat01petong <theerapat.ch@mail.wu.ac.th>
2024-06-22 15:21:28 +0700

    Initial commit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function Blog() {
+    return (
+        <div className="flex justify-center">
+            <div className="m-6 h-[100px] w-[500px] rounded-lg bg-slate-200 text-center ">
+                <div>title</div>
+                <div>details</div>
+            </div>
+        </div>
+    )
+}
